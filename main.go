bc0364a65f70d064c83c0cc223bed1ec7a456f66
taikicoco <kawasakitaiki0308@gmail.com>
2023-11-24 12:57:17 +0900

    update

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"log"
 	"os"
 	"server/scraping"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	var url string = os.Getenv("URL")
 	if url == "" {
 		log.Fatal("URL environment variable is not set")
